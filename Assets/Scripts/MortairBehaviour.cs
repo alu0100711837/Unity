df2752eab060fe867bd0af2949f0f0ba227e97e5
Alex <alu0100711837@ull.edu.es>
2018-10-28 20:46:25 +0000

    armas terminadas a falta de animacion

--- Assets/Scripts/MortairBehaviour.cs
+++ Assets/Scripts/MortairBehaviour.cs
@@ -15,7 +15,7 @@ public class MortairBehaviour : MonoBehaviour {
 	private AudioSource shotSound;
 	void Start () {
 		Cursor.lockState = CursorLockMode.Locked;	//Ocultamos el cursor
-		//shotSound = this.GetComponent<AudioSource>();
+		shotSound = this.GetComponent<AudioSource>();
 	}
 	
 	// Update is called once per frame
@@ -26,26 +26,34 @@ public class MortairBehaviour : MonoBehaviour {
 
 //Muy importante tener en cuenta el sistema de referencia de cada eje.
 	void Move() {
-		transform.Rotate(0f, Input.GetAxis("Mouse X") * speed, 0f, Space.World);
-		projectileSpawn.transform.Translate(Input.GetAxis("Vertical"), 0, -Input.GetAxis("Horizontal"));
+		if (timeUntilNextFire <= 0) {
+			transform.Rotate(0f, Input.GetAxis("Mouse X") * speed, 0f, Space.World);
+			projectileSpawn.transform.Translate(Input.GetAxis("Vertical"), 0, -Input.GetAxis("Horizontal"));
+		}
 	}
   
 	void Shoot() {
 		if (Input.GetMouseButton(0) && timeUntilNextFire <= 0) {
-				//Transform b = Instantiate(projectile, projectileSpawn.transform.position, projectileSpawn.transform.rotation);
+			StartCoroutine(FakeShoot());
 			StartCoroutine(dropBombs());
-				//Vector3 forceDirection = projectileSpawn.transform.position - this.transform.position;
-				//b.GetComponent<Rigidbody>().AddForce((forceDirection.normalized + desviation) * 400.0f);
-				//shotSound.Play();
 			timeUntilNextFire = timeBetweenFire;
 		}
 		timeUntilNextFire -= Time.deltaTime;
 	}
 
 	IEnumerator dropBombs() {
+		yield return new WaitForSeconds(3.0f);
 		for (int i = 0; i < 5; i++) {
 			Vector3 desviation = new Vector3(Random.Range(-error, error), 0, Random.Range(-error, error));
 			Transform b = Instantiate(projectile, projectileSpawn.transform.position + desviation, projectileSpawn.transform.rotation);
+			b.GetComponent<Rigidbody>().AddForce(new Vector3(0, -500000.0f, 0));
+			yield return new WaitForSeconds(0.5f);
+		}
+	}
+
+	IEnumerator FakeShoot() {
+		for (int i = 0; i < 5; i++) {
+			shotSound.Play();
 			yield return new WaitForSeconds(0.5f);
 		}
 	}
